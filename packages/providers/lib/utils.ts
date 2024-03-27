b4a4486a8cc1e5d6aa1c3813265627115c80bcad
Josh Schneider <joshdschneider@Joshs-MacBook-Pro.local>
2024-03-27 14:28:31 -0400

    updated google drive

--- packages/providers/lib/utils.ts
+++ packages/providers/lib/utils.ts
@@ -2,13 +2,11 @@ import { createClient } from '@deepgram/sdk';
 import dotenv from 'dotenv';
 dotenv.config();
 
-export function getDeepgramApiKey() {
+export function getDeepgramInstance() {
   const apiKey = process.env['DEEPGRAM_API_KEY'];
   if (!apiKey) {
     throw new Error('Deepgram API key not set');
   }
 
-  return apiKey;
+  return createClient(apiKey);
 }
-
-const deepgram = createClient(getDeepgramApiKey());
