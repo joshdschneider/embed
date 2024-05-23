d90a49e3a858c5bc61932b689f3e3f49553df61f
Josh Schneider <joshds313@gmail.com>
2024-05-23 14:41:05 -0400

    hotfix cors error

--- packages/server/lib/utils/cors.ts
+++ packages/server/lib/utils/cors.ts
@@ -1,6 +1,6 @@
 import { CorsOptions } from 'cors';
 
 export const corsOptions: CorsOptions = {
-  origin: ['http://localhost:3000', 'https://app.embed.com'],
+  origin: ['http://localhost:3000', 'https://app.useembed.com'],
   credentials: true,
 };
